00b0cf9552645b51e8bfed7439b0705ac80f8b06
rideshs603-s <rideshs603@gmail.com>
2025-10-07 19:38:28 +0545

    Create utils.c

---
+++ src/utils.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Safe integer input
+int getInt() {
+    int n;
+    while(scanf("%d", &n) != 1) {
+        while(getchar() != '\n'); // clear invalid input
+        printf("Invalid input, enter an integer: ");
+    }
+    return n;
+}
+
+// Safe float input
+float getFloat() {
+    float f;
+    while(scanf("%f", &f) != 1) {
+        while(getchar() != '\n');
+        printf("Invalid input, enter a number: ");
+    }
+    return f;
+}

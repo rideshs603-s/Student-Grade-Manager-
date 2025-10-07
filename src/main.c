d09b6c5edf72764df31ea5ae088b790ac1d76518
rideshs603-s <rideshs603@gmail.com>
2025-10-07 19:39:25 +0545

    Update main.c

--- src/main.c
+++ src/main.c
@@ -7,15 +7,18 @@ int main() {
     int count = 0;
     int choice;
 
-    while (1) {
+    while(1) {
         printf("\n===== Student Grade Management System =====\n");
         printf("1. Add Student\n");
         printf("2. Display Students\n");
-        printf("3. Exit\n");
+        printf("3. Search Student\n");
+        printf("4. Update Student\n");
+        printf("5. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
-        switch (choice) {
+        int roll;
+        switch(choice) {
             case 1:
                 addStudent(students, &count);
                 break;
@@ -23,6 +26,16 @@ int main() {
                 displayStudents(students, count);
                 break;
             case 3:
+                printf("Enter roll to search: ");
+                scanf("%d", &roll);
+                searchStudent(students, count, roll);
+                break;
+            case 4:
+                printf("Enter roll to update: ");
+                scanf("%d", &roll);
+                updateStudent(students, count, roll);
+                break;
+            case 5:
                 printf("Exiting program...\n");
                 exit(0);
             default:

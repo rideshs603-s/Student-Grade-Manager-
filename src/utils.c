cb0e86a0cea99c671dd778e705218e9f755b6216
rideshs603-s <rideshs603@gmail.com>
2025-10-07 20:21:09 +0545

    Added search and update student functionality

--- src/utils.c
+++ src/utils.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 
-
-// Safe integer input
 int getInt() {
     int n;
     while(scanf("%d", &n) != 1) {
-        while(getchar() != '\n'); // clear invalid input
+        while(getchar() != '\n');
         printf("Invalid input, enter an integer: ");
     }
     return n;
 }
 
-// Safe float input
 float getFloat() {
     float f;
     while(scanf("%f", &f) != 1) {

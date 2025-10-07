af781afa80012e3b8787bbd755fddb5661b164c0
rideshs603-s <rideshs603@gmail.com>
2025-10-07 20:16:53 +0545

    Added Student struct and basic menu

--- src/main.c
+++ src/main.c
@@ -1,48 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "../include/student.h"
 
+typedef struct {
+    int roll;
+    char name[50];
+    float marks;
+    char grade;
+} Student;
 
 int main() {
-    Student students[100];
-    int count = 0;
-    int choice;
-
-    while(1) {
-        printf("\n===== Student Grade Management System =====\n");
-        printf("1. Add Student\n");
-        printf("2. Display Students\n");
-        printf("3. Search Student\n");
-        printf("4. Update Student\n");
-        printf("5. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        int roll;
-        switch(choice) {
-            case 1:
-                addStudent(students, &count);
-                break;
-            case 2:
-                displayStudents(students, count);
-                break;
-            case 3:
-                printf("Enter roll to search: ");
-                scanf("%d", &roll);
-                searchStudent(students, count, roll);
-                break;
-            case 4:
-                printf("Enter roll to update: ");
-                scanf("%d", &roll);
-                updateStudent(students, count, roll);
-                break;
-            case 5:
-                printf("Exiting program...\n");
-                exit(0);
-            default:
-                printf("Invalid choice! Try again.\n");
-        }
-    }
-
+    printf("===== Student Grade Management System =====\n");
+    printf("1. Add Student\n2. Display Students\n3. Exit\n");
     return 0;
 }

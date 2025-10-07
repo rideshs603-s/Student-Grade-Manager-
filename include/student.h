fe7a86bfd338990bcb8f9a1dd0e2ec03bbe339e0
rideshs603-s <rideshs603@gmail.com>
2025-10-07 20:18:05 +0545

    Implemented add and display student functions

--- include/student.h
+++ include/student.h
@@ -1,7 +1,6 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
-
 typedef struct {
     int roll;
     char name[50];
@@ -9,13 +8,8 @@ typedef struct {
     char grade;
 } Student;
 
-// Phase 2 functions
 void addStudent(Student students[], int *count);
 void displayStudents(Student students[], int count);
 char calculateGrade(float marks);
-
-// Phase 3 functions
-void searchStudent(Student students[], int count, int roll);
-void updateStudent(Student students[], int count, int roll);
 
 #endif

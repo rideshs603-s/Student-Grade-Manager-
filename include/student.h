850026a1bba86cf580c7742071298d8b4d9b5990
rideshs603-s <rideshs603@gmail.com>
2025-10-07 19:37:12 +0545

    Update student.h

--- include/student.h
+++ include/student.h
@@ -8,4 +8,13 @@ typedef struct {
     char grade;
 } Student;
 
+// Phase 2 functions
+void addStudent(Student students[], int *count);
+void displayStudents(Student students[], int count);
+char calculateGrade(float marks);
+
+// Phase 3 functions
+void searchStudent(Student students[], int count, int roll);
+void updateStudent(Student students[], int count, int roll);
+
 #endif
